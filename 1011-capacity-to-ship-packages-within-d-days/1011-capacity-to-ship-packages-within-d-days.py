0c3930ade75c8f0502ff70cc090c2a82d0f8c80e
Kajal Kumari <kajal13march05@gmail.com>
2026-07-10 10:57:11 +0530

    Time: 178 ms (94.64%), Space: 23.7 MB (93.44%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,29 +1,41 @@
 from typing import List
 
 class Solution:
+
+    def numberOfDays(self, shipWeight: int, arr: List[int]) -> int:
+        day = 1
+        packageWeight = 0
+
+        for weight in arr:
+            packageWeight += weight
+
+            if packageWeight > shipWeight:
+                day += 1
+                packageWeight = weight
+
+        return day
+
+    def binarySearch(self, minWeight: int, maxWeight: int, arr: List[int], days: int) -> int:
+        l = minWeight
+        r = maxWeight
+        minCapacity = float('inf')
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            tempDay = self.numberOfDays(mid, arr)
+
+            if tempDay <= days:
+                minCapacity = min(minCapacity, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return minCapacity
+
     def shipWithinDays(self, weights: List[int], days: int) -> int:
 
-        def canShip(capacity):
-            day = 1
-            load = 0
+        minWeightOfShip = max(weights)
+        maxWeightOfShip = sum(weights)
 
-            for w in weights:
-                if load + w > capacity:
-                    day += 1
-                    load = 0
-                load += w
-
-            return day <= days
-
-        left = max(weights)
-        right = sum(weights)
-
-        while left < right:
-            mid = (left + right) // 2
-
-            if canShip(mid):
-                right = mid
-            else:
-                left = mid + 1
-
-        return left
+        return self.binarySearch(minWeightOfShip, maxWeightOfShip, weights, days)
