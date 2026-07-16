81aec36ad501ddd6288083657767bc3f01111c5f
Kajal Kumari <kajal13march05@gmail.com>
2026-07-16 06:23:30 +0530

    Time: 36 ms (29.43%), Space: 14.4 MB (8.17%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,18 +1,18 @@
 class Solution:
     def findErrorNums(self, arr):
-        arr.sort()
         n = len(arr)
+        freq = {}
+
+        for num in arr:
+            freq[num] = freq.get(num, 0) + 1
 
         repeat = -1
-        missing = 1
+        missing = -1
 
-        for i in range(1, n):
-            if arr[i] == arr[i - 1]:
-                repeat = arr[i]
-            elif arr[i] > arr[i - 1] + 1:
-                missing = arr[i - 1] + 1
-
-        if arr[-1] != n:
-            missing = n
+        for i in range(1, n + 1):
+            if freq.get(i, 0) == 2:
+                repeat = i
+            elif freq.get(i, 0) == 0:
+                missing = i
 
         return [repeat, missing]
