782bd0e819a21153b893c4f8feef5175cfad89eb
Kajal Kumari <kajal13march05@gmail.com>
2026-07-24 07:32:07 +0530

    Time: 52 ms (12.86%), Space: 28.5 MB (6.35%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self,nums): 
+        freq={}
+        
+        for num in nums:
+            freq[num] = freq.get(num,0) +1
+
+            if freq[num] > 1:
+                return True
+
+        return False        
