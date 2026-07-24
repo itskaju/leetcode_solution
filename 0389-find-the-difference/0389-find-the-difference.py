030c36ec02d1d525f6ca594c4f8963e89cb6e6ca
Kajal Kumari <kajal13march05@gmail.com>
2026-07-24 11:05:02 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.78%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,15 +1,6 @@
-class Solution(object):
-    def findTheDifference(self, s, t):
-        
-        freq = {}
-
-        for ch in s:
-            freq[ch]=freq.get(ch,0) + 1
-
-
-        for ch in t:
-
-            if ch not in freq or freq[ch] == 0:
-               return ch
-
-            freq[ch]-=1       
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        result = 0
+        for ch in s + t:   # combine both strings
+            result ^= ord(ch)   # XOR ASCII value of character
+        return chr(result)      # convert back to character
