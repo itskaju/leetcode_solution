9777181f92c422a2805380c76faff4656c1d784b
Kajal Kumari <kajal13march05@gmail.com>
2026-08-16 07:25:55 +0530

    Time: 0 ms (100%), Space: 13.2 MB (72.01%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
